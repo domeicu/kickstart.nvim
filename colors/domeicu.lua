3f3ae127ec9f5261fa4d3bf9e9eab0c7c0b27ca9
domeicu <domenicseto@gmail.com>
2026-04-24 11:49:46 +1000

    update colour naming

--- colors/domeicu.lua
+++ colors/domeicu.lua
@@ -7,14 +7,13 @@ vim.o.termguicolors = true
 vim.g.colors_name = "domeicu"
 
 local palette = {
-  bg_dark      = "#070707", -- Activity bar / absolute darkest
+  bg_deep      = "#070707", -- Activity bar / absolute darkest
   bg_base      = "#0E0E0D", -- Editor / sidebar / terminal
-  fg_cream     = "#CCC8C2", -- Primary text / variables
-  fg_mocha     = "#6B6B68", -- Comments / secondary text
-  matcha_acc   = "#F1FFD9", -- Keywords / Strings / Functions
-  matcha_sub   = "#C0CFA7", -- Numbers / Constants / Structs / Types
-  selection    = "#1E1E1C", -- Visual selection
-  matcha_trans = "#171F0E", -- Word highlight background
+  fg_primary   = "#CCC8C2", -- Primary text / variables
+  fg_muted     = "#6B6B68", -- Comments / secondary text
+  accent       = "#F1FFD9", -- Keywords / Strings / Functions
+  accent_dim   = "#C0CFA7", -- Numbers / Constants / Structs / Types
+  selection    = "#4B4B48", -- Visual selection
 }
 
 local function hi(group, opts)
@@ -24,89 +23,89 @@ end
 -- =========================================
 -- UI Elements
 -- =========================================
-hi("Normal",       { fg = palette.fg_cream, bg = palette.bg_base })
-hi("NormalFloat",  { fg = palette.fg_cream, bg = palette.bg_dark })
-hi("FloatBorder",  { fg = palette.fg_mocha, bg = palette.bg_dark })
-hi("ColorColumn",  { bg = palette.bg_dark })
-hi("Cursor",       { fg = palette.bg_base, bg = palette.matcha_acc })
-hi("CursorLine",   { bg = palette.bg_dark })
-hi("CursorLineNr", { fg = palette.matcha_acc, bg = palette.bg_dark, bold = true })
-hi("LineNr",       { fg = palette.fg_mocha, bg = palette.bg_base })
+hi("Normal",       { fg = palette.fg_primary, bg = palette.bg_base })
+hi("NormalFloat",  { fg = palette.fg_primary, bg = palette.bg_deep })
+hi("FloatBorder",  { fg = palette.fg_muted,   bg = palette.bg_deep })
+hi("ColorColumn",  { bg = palette.bg_deep })
+hi("Cursor",       { fg = palette.bg_base,    bg = palette.accent })
+hi("CursorLine",   { bg = palette.bg_deep })
+hi("CursorLineNr", { fg = palette.accent,     bg = palette.bg_deep, bold = true })
+hi("LineNr",       { fg = palette.fg_muted,   bg = palette.bg_base })
 hi("SignColumn",   { bg = palette.bg_base })
 hi("Visual",       { bg = palette.selection })
-hi("Search",       { fg = palette.bg_base, bg = palette.matcha_acc })
-hi("IncSearch",    { fg = palette.bg_base, bg = palette.matcha_sub })
-hi("VertSplit",    { fg = palette.bg_dark, bg = palette.bg_base })
-hi("WinSeparator", { fg = palette.bg_dark, bg = palette.bg_base })
-hi("StatusLine",   { fg = palette.fg_cream, bg = palette.bg_dark })
-hi("StatusLineNC", { fg = palette.fg_mocha, bg = palette.bg_dark })
-hi("Pmenu",        { fg = palette.fg_cream, bg = palette.bg_dark })
-hi("PmenuSel",     { fg = palette.bg_base, bg = palette.matcha_acc })
-hi("MatchParen",   { fg = palette.matcha_acc, bg = palette.matcha_trans, bold = true })
-hi("Directory",    { fg = palette.matcha_acc })
+hi("Search",       { fg = palette.bg_base,    bg = palette.accent })
+hi("IncSearch",    { fg = palette.bg_base,    bg = palette.accent_dim })
+hi("VertSplit",    { fg = palette.bg_deep,    bg = palette.bg_base })
+hi("WinSeparator", { fg = palette.bg_deep,    bg = palette.bg_base })
+hi("StatusLine",   { fg = palette.fg_primary, bg = palette.bg_deep })
+hi("StatusLineNC", { fg = palette.fg_muted,   bg = palette.bg_deep })
+hi("Pmenu",        { fg = palette.fg_primary, bg = palette.bg_deep })
+hi("PmenuSel",     { fg = palette.bg_base,    bg = palette.accent })
+hi("MatchParen",   { fg = palette.accent,     bg = palette.selection, bold = true })
+hi("Directory",    { fg = palette.accent })
 
 -- =========================================
 -- Syntax Highlighting
 -- =========================================
-hi("Comment",      { fg = palette.fg_mocha, italic = true })
-hi("String",       { fg = palette.matcha_acc })
-hi("Number",       { fg = palette.matcha_sub })
-hi("Float",        { fg = palette.matcha_sub })
-hi("Boolean",      { fg = palette.matcha_sub })
-hi("Constant",     { fg = palette.matcha_sub })
+hi("Comment",      { fg = palette.fg_muted,   italic = true })
+hi("String",       { fg = palette.accent })
+hi("Number",       { fg = palette.accent_dim })
+hi("Float",        { fg = palette.accent_dim })
+hi("Boolean",      { fg = palette.accent_dim })
+hi("Constant",     { fg = palette.accent_dim })
 
-hi("Identifier",   { fg = palette.fg_cream })
-hi("Function",     { fg = palette.matcha_acc })
-hi("Statement",    { fg = palette.matcha_acc })
-hi("Conditional",  { fg = palette.matcha_acc })
-hi("Repeat",       { fg = palette.matcha_acc })
-hi("Operator",     { fg = palette.fg_cream })
-hi("Keyword",      { fg = palette.matcha_acc })
-hi("Exception",    { fg = palette.matcha_acc })
+hi("Identifier",   { fg = palette.fg_primary })
+hi("Function",     { fg = palette.accent })
+hi("Statement",    { fg = palette.accent })
+hi("Conditional",  { fg = palette.accent })
+hi("Repeat",       { fg = palette.accent })
+hi("Operator",     { fg = palette.fg_primary })
+hi("Keyword",      { fg = palette.accent })
+hi("Exception",    { fg = palette.accent })
 
-hi("PreProc",      { fg = palette.matcha_acc })
-hi("Type",         { fg = palette.matcha_sub })
-hi("Structure",    { fg = palette.matcha_sub })
-hi("Typedef",      { fg = palette.matcha_sub })
+hi("PreProc",      { fg = palette.accent })
+hi("Type",         { fg = palette.accent_dim })
+hi("Structure",    { fg = palette.accent_dim })
+hi("Typedef",      { fg = palette.accent_dim })
 
-hi("Special",      { fg = palette.matcha_acc })
+hi("Special",      { fg = palette.accent })
 hi("Underlined",   { underline = true })
 hi("Error",        { fg = palette.bg_base, bg = "#FF5370" })
-hi("Todo",         { fg = palette.matcha_acc, bg = palette.bg_dark, bold = true })
+hi("Todo",         { fg = palette.accent,  bg = palette.bg_deep, bold = true })
 
 -- =========================================
 -- Treesitter (Neovim 0.8+)
 -- =========================================
-hi("@variable",          { fg = palette.fg_cream })
-hi("@variable.builtin",  { fg = palette.matcha_sub, italic = true })
-hi("@function",          { fg = palette.matcha_acc })
-hi("@function.builtin",  { fg = palette.matcha_acc, italic = true })
-hi("@keyword",           { fg = palette.matcha_acc })
-hi("@operator",          { fg = palette.fg_cream })
-hi("@type",              { fg = palette.matcha_sub })
-hi("@type.builtin",      { fg = palette.matcha_sub })
-hi("@property",          { fg = palette.matcha_sub })
-hi("@parameter",         { fg = palette.matcha_sub })
-hi("@punctuation.bracket",{ fg = palette.fg_cream })
-hi("@punctuation.delimiter",{ fg = palette.fg_cream })
-hi("@string",            { fg = palette.matcha_acc })
-hi("@number",            { fg = palette.matcha_sub })
-hi("@boolean",           { fg = palette.matcha_sub })
-hi("@constant",          { fg = palette.matcha_sub })
-hi("@tag",               { fg = palette.matcha_acc })
-hi("@tag.attribute",     { fg = palette.matcha_sub })
+hi("@variable",             { fg = palette.fg_primary })
+hi("@variable.builtin",     { fg = palette.accent_dim, italic = true })
+hi("@function",             { fg = palette.accent })
+hi("@function.builtin",     { fg = palette.accent,     italic = true })
+hi("@keyword",              { fg = palette.accent })
+hi("@operator",             { fg = palette.fg_primary })
+hi("@type",                 { fg = palette.accent_dim })
+hi("@type.builtin",         { fg = palette.accent_dim })
+hi("@property",             { fg = palette.accent_dim })
+hi("@parameter",            { fg = palette.accent_dim })
+hi("@punctuation.bracket",  { fg = palette.fg_primary })
+hi("@punctuation.delimiter",{ fg = palette.fg_primary })
+hi("@string",               { fg = palette.accent })
+hi("@number",               { fg = palette.accent_dim })
+hi("@boolean",              { fg = palette.accent_dim })
+hi("@constant",             { fg = palette.accent_dim })
+hi("@tag",                  { fg = palette.accent })
+hi("@tag.attribute",        { fg = palette.accent_dim })
 
 -- =========================================
 -- Common Plugins (Telescope, NvimTree)
 -- =========================================
-hi("TelescopeNormal",       { fg = palette.fg_cream })
-hi("TelescopeBorder",       { fg = palette.fg_mocha })
-hi("TelescopePromptNormal", { fg = palette.fg_cream })
-hi("TelescopePromptBorder", { fg = palette.fg_mocha })
+hi("TelescopeNormal",       { fg = palette.fg_primary })
+hi("TelescopeBorder",       { fg = palette.fg_muted })
+hi("TelescopePromptNormal", { fg = palette.fg_primary })
+hi("TelescopePromptBorder", { fg = palette.fg_muted })
 hi("TelescopeSelection",    { bg = palette.selection })
 
-hi("NvimTreeNormal",        { fg = palette.fg_cream, bg = palette.bg_base })
-hi("NvimTreeEndOfBuffer",   { fg = palette.bg_base, bg = palette.bg_base })
-hi("NvimTreeVertSplit",     { fg = palette.bg_dark, bg = palette.bg_base })
-hi("NvimTreeFolderName",    { fg = palette.matcha_acc })
-hi("NvimTreeOpenedFile",    { fg = palette.matcha_acc, bold = true })
+hi("NvimTreeNormal",        { fg = palette.fg_primary, bg = palette.bg_base })
+hi("NvimTreeEndOfBuffer",   { fg = palette.bg_base,    bg = palette.bg_base })
+hi("NvimTreeVertSplit",     { fg = palette.bg_deep,    bg = palette.bg_base })
+hi("NvimTreeFolderName",    { fg = palette.accent })
+hi("NvimTreeOpenedFile",    { fg = palette.accent,     bold = true })
